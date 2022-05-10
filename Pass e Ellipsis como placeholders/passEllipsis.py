bbb521deb8b59ece2bdc51c5cd654ad3e55c5f44
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-05-10 10:42:13 -0300

    Pass e Ellipsis como placeholders

---
+++ Pass e Ellipsis como placeholders/passEllipsis.py
@@ -0,0 +1,15 @@
+valor = False;
+
+if valor:
+    pass # deixando o lugar vago para escrever futuramente
+else:
+    print('Tchau')
+
+# é possível usar o ellipsis para fazer o mesmo procedimento, mesmo não tendo sido feito pra isso
+
+valor = True;
+
+if valor:
+    ... # deixando o lugar vago para escrever futuramente
+else:
+    print('Tchau')
