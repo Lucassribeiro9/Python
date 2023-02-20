a5f0d0a2176af42567c48e6f0cc8e734c7a20497
Lucassribeiro9 <57766036+Lucassribeiro9@users.noreply.github.com>
2023-02-20 19:05:00 +0000

    fix**

--- ex_ifs/ex01.py
+++ ex_ifs/ex01.py
@@ -4,13 +4,15 @@ informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informe que não é um número inteiro.
 """
 
-inteiro = int(input('Digite um número inteiro: '))
+inteiro = input('Digite um número inteiro: ')
 
-if inteiro is int:
-    if inteiro % 2 == 0:
-       print('Par')
-    else:
-        print('Impar')    
-else:
-    print('Digite um número inteiro')
+try:
+    entrada_int = int(inteiro)
+    par_impar = entrada_int % 2 == 0
+    par_impar_texto = 'ímpar'
 
+    if par_impar:
+        par_impar_texto = 'par'
+    print(f'O número {entrada_int} é {par_impar_texto}')
+except:
+    print('Você não digitou um número inteiro')
