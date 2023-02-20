6a097beb7cf82b5cf0880a7bd71b06a6e53f6048
Lucassribeiro9 <57766036+Lucassribeiro9@users.noreply.github.com>
2023-02-20 19:13:51 +0000

    fix**

--- ex_ifs/ex02.py
+++ ex_ifs/ex02.py
@@ -4,11 +4,11 @@ descrito, exiba a saudação apropriada. Ex.
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
 
-hora = input('Que horas são? ')
-
-if hora => 0 and hora <=11:
+entrada = input('Que horas são? ')
+hora = int(entrada)
+if hora >= 0 and hora <=11:
     print('Bom dia!')
-elif hora => 12 and hora <=17:
+elif hora >= 12 and hora <=17:
     print('Boa tarde')
 elif hora >= 18 and hora <= 23:
     print('Boa noite')
