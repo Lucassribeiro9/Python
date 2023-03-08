6fcec1b16623d8068f1d074563dbee935b68e388
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-03-08 11:15:42 -0300

    .

---
+++ exercicios pt1/ex01.py
@@ -0,0 +1,11 @@
+num1 = print('Digite um número inteiro: ')
+
+if num1.isdigit():
+    num1 = int(num1)
+else:    
+    print('Digite um número válido.')
+
+if num1 % 2 == 0:
+    print(f'O número é par: {num1}')
+else:
+    print(f'O número é ímpar: {num1}')
