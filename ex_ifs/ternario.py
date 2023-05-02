6547689ab5c7c829085a59928c244c3bde292021
Lucassribeiro9 <57766036+Lucassribeiro9@users.noreply.github.com>
2023-05-02 18:55:20 +0000

    operadores ternarios

---
+++ ex_ifs/ternario.py
@@ -0,0 +1,11 @@
+"""
+Operações ternárias (if else em uma linha)
+"""
+
+condicao = 10 == 11
+variavel = 'Valor' if condicao else 'Outro valor'
+print(variavel)
+
+digito = 9
+novo_digito = digito if digito <=9 else 0
+print(novo_digito)
