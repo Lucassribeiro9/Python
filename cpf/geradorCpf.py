06d82a39d56ac7196829eaa3de033b9fe55f3603
Lucassribeiro9 <57766036+Lucassribeiro9@users.noreply.github.com>
2023-05-03 19:19:02 +0000

    gerando primeiro número cpf

---
+++ cpf/geradorCpf.py
@@ -0,0 +1,11 @@
+cpf = '74682489070'
+nove_digitos = cpf[:9]
+cont_regressivo = 10
+resultado = 0
+
+for digito in nove_digitos:
+    resultado += int(digito) * cont_regressivo
+    cont_regressivo -= 1
+digito = (resultado * 10) % 11
+digito = digito if digito <= 9 else 0
+print(digito)    
