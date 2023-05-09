f6e4d18c6e5dcde53fdca3a4dfa7f3ad6ac5d11e
Lucassribeiro9 <57766036+Lucassribeiro9@users.noreply.github.com>
2023-05-09 14:16:58 +0000

    validacao basica

--- cpf/geradorCpf.py
+++ cpf/geradorCpf.py
@@ -1,5 +1,5 @@
-cpf = '74682489070'
-nove_digitos = cpf[:9]
+cpf_usuario = '74682489070'
+nove_digitos = cpf_usuario[:9]
 cont_regressivo_1 = 10
 resultado_dig1 = 0
 
@@ -8,9 +8,8 @@ for digito in nove_digitos:
     cont_regressivo_1 -= 1
 digito_1 = (resultado_dig1 * 10) % 11
 digito_1 = digito_1 if digito_1 <= 9 else 0
-print(digito_1)
 
-dez_digitos = cpf[:9] + str(digito)
+dez_digitos = cpf_usuario[:9] + str(digito)
 cont_regressivo_2 = 11
 resultado_dig2 = 0
 
@@ -19,3 +18,10 @@ for digito in dez_digitos:
     cont_regressivo_2 -= 1
 digito_2 = (resultado_dig2 * 10) % 11
 digito_2 = digito_2 if digito_2 <= 9 else 0
+
+cpf_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+
+if cpf_usuario == cpf_calculo:
+    print(f'{cpf_usuario} é válido')
+else:
+    print('CPF inválido')
