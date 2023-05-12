d8420a2b861adfb2cbe31a201b130039e9c17198
Lucassribeiro9 <57766036+Lucassribeiro9@users.noreply.github.com>
2023-05-12 20:51:00 +0000

    import sys e copia do codigo da aula

--- cpf/geradorCpf.py
+++ cpf/geradorCpf.py
@@ -1,31 +1,47 @@
+# cpf = '36440847007'  # Esse CPF gera o primeiro dígito como 10 (0)
 import re
+import sys
 
+# cpf_enviado_usuario = '746.824.890-70' \
+#     .replace('.', '') \
+#     .replace(' ', '') \
+#     .replace('-', '')
+entrada = input('CPF [746.824.890-70]: ')
+cpf_enviado_usuario = re.sub(
+    r'[^0-9]',
+    '',
+    entrada
+)
 
-cpf_usuario = '746.824.890-70'
-cpf_usuario = re.sub(r'[^0-9]', '', cpf_usuario)
-nove_digitos = cpf_usuario[:9]
-cont_regressivo_1 = 10
-resultado_dig1 = 0
+entrada_e_sequencial = entrada == entrada[0] * len(entrada)
 
+if entrada_e_sequencial:
+    print('Você enviou dados sequenciais.')
+    sys.exit()
+
+nove_digitos = cpf_enviado_usuario[:9]
+contador_regressivo_1 = 10
+
+resultado_digito_1 = 0
 for digito in nove_digitos:
-    resultado_dig1 += int(digito) * cont_regressivo_1
-    cont_regressivo_1 -= 1
-digito_1 = (resultado_dig1 * 10) % 11
+    resultado_digito_1 += int(digito) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+digito_1 = (resultado_digito_1 * 10) % 11
 digito_1 = digito_1 if digito_1 <= 9 else 0
 
-dez_digitos = cpf_usuario[:9] + str(digito)
-cont_regressivo_2 = 11
-resultado_dig2 = 0
+dez_digitos = nove_digitos + str(digito_1)
+contador_regressivo_2 = 11
 
+resultado_digito_2 = 0
 for digito in dez_digitos:
-    resultado_dig2 = int(digito)*cont_regressivo_2
-    cont_regressivo_2 -= 1
-digito_2 = (resultado_dig2 * 10) % 11
+    resultado_digito_2 += int(digito) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+digito_2 = (resultado_digito_2 * 10) % 11
 digito_2 = digito_2 if digito_2 <= 9 else 0
 
-cpf_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
 
-if cpf_usuario == cpf_calculo:
-    print(f'{cpf_usuario} é válido')
+if cpf_enviado_usuario == cpf_gerado_pelo_calculo:
+    print(f'{cpf_enviado_usuario} é válido')
 else:
     print('CPF inválido')
