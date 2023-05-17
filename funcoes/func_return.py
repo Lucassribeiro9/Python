1dcb80ccb7c9267c40e11cc06830d175937f6174
Lucassribeiro9 <57766036+Lucassribeiro9@users.noreply.github.com>
2023-05-17 18:28:55 +0000

    func return 2

--- funcoes/func_return.py
+++ funcoes/func_return.py
@@ -1,6 +1,10 @@
-def soma(x, y):
-    resultado = x + y
-    return resultado
+import sys
 
-resultado = soma(10, 20)
-print(resultado)
+def soma(x, y):
+    if x or y <= 0:
+        print("Invalid")
+    return x + y
+
+resultado_1 = soma(3, 3)
+resultado_2 = soma(7, 2)
+print(soma(1, 5))
