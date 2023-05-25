e75c4c135ec9ffbfb42a864f707d44c6cee5b1b6
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-05-25 14:41:45 -0300

    passando valores ao dict

--- dict/intro_dict.py
+++ dict/intro_dict.py
@@ -1,16 +1,4 @@
-pessoa = {
- 'nome': 'Lucas',
- 'sobrenome': 'Ribeiro',
- 'idade': 28,
- 'altura': 1.9,
- 'endereços': [
-	 {'rua': 'tal tal', 'número': 123},
-	 {'rua': 'outra rua', 'número': 321},
- ] # lista com dict
-}
-print(pessoa)
-print()
-print(pessoa['nome'])
+pessoa = {}
 
-for chave in pessoa:
-    print(chave, pessoa[chave])
+pessoa['name'] = 'Lucas'
+print(pessoa['name'])
