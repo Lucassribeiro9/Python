a310ebdd30890c1efd94b0d1cc017283a782864c
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-05-29 18:58:21 -0300

    operadores

---
+++ set/operadores.py
@@ -0,0 +1,14 @@
+s1 = {1, 2, 3}
+s2 = {2, 3, 4}
+# uniao
+s3 = s1 | s2
+# intersecçao
+s4 = s1 & s2
+# diferença
+s5 = s2 - s1
+# diferença simetrica
+s6 = s1 ^ s2
+print(s3)
+print(s4)
+print(s5)
+print(s6) 
