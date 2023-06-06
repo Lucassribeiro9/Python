9a8027cd8f95bc93a83bfb976d6ed72ebc943140
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-06-06 10:08:04 -0300

    update

--- dict/dict_exercicios/ex06.py
+++ dict/dict_exercicios/ex06.py
@@ -1,6 +1,6 @@
 import random
 
-lista = [random.randint(0,60)]
+lista = [random.randint(1,60) for i in range(10)]
 
 
 for i in lista:
@@ -8,3 +8,16 @@ for i in lista:
         print(i)
 print("--------------------------------")
 print(lista)    
+
+# outro exemplo que pode ser feito com listas dentro de listas
+
+def lista_usuario(tamanho):
+    lst = []
+    for i in range(tamanho):
+        sub_lst = []
+        for i in range(10):
+            sub_lst.append(random.randint(1,50))
+        lst.append(sub_lst)
+    return lst
+
+print(lista_usuario(8))
