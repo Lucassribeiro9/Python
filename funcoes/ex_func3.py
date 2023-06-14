e4d8710ba9ce003381b3b0d60fbd0e6f956a4077
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-06-14 20:44:21 -0300

    teste de IA

--- funcoes/ex_func3.py
+++ funcoes/ex_func3.py
@@ -1,4 +1,14 @@
 def multiplicador(multiplicador):
+    """
+    This function returns the inner function multiplicar, which is a closure
+    function that will multiply its argument by the value of the input parameter
+    multiplicador. The input parameter multiplicador is a number that will be
+    used as the multiplication factor. The inner function multiplicar takes
+    one parameter, numero, which is the number to be multiplied. The return
+    value of the inner function is the multiplication of numero and the
+    multiplication factor. The return type of the inner function is the same
+    as the type of the input parameter numero.
+    """
     def multiplicar(numero):
         return numero * multiplicador
     return multiplicar
