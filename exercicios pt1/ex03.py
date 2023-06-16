bbee0de8ea796146ea9a9905951470a6c108478a
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-06-15 22:19:08 -0300

    update ex03

--- exercicios pt1/ex03.py
+++ exercicios pt1/ex03.py
@@ -1,29 +1,56 @@
 """Faça um programa que utilize um dicionário para armazenar as notas de um aluno em
-três disciplinas: Matemática, Português e História.
+n disciplinas.
 Em seguida, o programa deve calcular a média das notas e imprimir o resultado."""
 import os
+
+
 def mediaNotas(notas):
     media = sum(notas.values()) / len(notas)
     return media
+
+
 def isAprovado(nota):
     if nota >= 5:
-        return print('Aprovado')
+        return print("Aprovado")
     else:
-        return print('Reprovado')
+        return print("Reprovado")
+
+
 notas = {}
+
 while True:
-    qtd_materias = int(input('Digite a quantidade de materias: '))
+    while True:
+        try:
+            qtd_materias = int(input("Digite a quantidade de materias: "))
+            if qtd_materias <= 0:
+                print("A quantidade de materias deve ser maior que zero.")
+                continue
+            break
+        except ValueError:
+            print("Valor inválido")
     for i in range(qtd_materias):
-        materia = input('Digite a materia: ')
-        nota = float(input('Digite a nota: '))
-        notas[materia] = nota    
+        while True:
+            materia = input("Digite a materia: ")
+            if isinstance(materia, str):
+                break
+            else:
+                print("Materia inválida.")
+
+        while True:
+            try:
+                nota = float(input("Digite a nota: "))
+                break
+            except ValueError:
+                print("Valor inválido")
+        notas[materia] = nota
     mediaNotas(notas)
+    print(f"Média das notas: {mediaNotas(notas):.2f}")
     isAprovado(mediaNotas(notas))
-    resposta = input('Deseja continuar? S/N: ')
-    if resposta == 'N':
+    resposta = input("Deseja continuar? S/N: ").upper()
+    if resposta == "N":
+        print("Programa encerrado.")
         break
     else:
-        os.system('cls')
+        os.system("cls")
+        notas = {}
         continue
-
-
