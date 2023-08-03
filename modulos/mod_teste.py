c255f0881120a825dba612575092b5eaf14f21fc
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-08-03 16:57:25 -0300

    testando modularizacao

---
+++ modulos/mod_teste.py
@@ -0,0 +1,3 @@
+print('Esse módulo se chama: ', __name__)
+def soma(x, y):
+    return x + y
---
+++ modulos/uso_mod.py
@@ -0,0 +1,6 @@
+import mod_teste
+from mod_teste import soma
+print('Esse módulo se chama: ', __name__)
+
+print(mod_teste.soma(1, 2))
+print(soma(6,6))
