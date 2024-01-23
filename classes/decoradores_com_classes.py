771986bf46299af895f4a3b5414f475dc16b213b
Lucas Ribeiro <lucas.devlop94@gmail.com>
2024-01-23 15:31:13 -0300

    add decoradores

--- classes/decoradores_com_classes.py
+++ classes/decoradores_com_classes.py
@@ -6,6 +6,15 @@ def meu_repr(self):
         class_dict = self.__dict__
         class_repr = f'{class_name}({class_dict})'
         return class_repr
+def meu_planeta(metodo):
+    def interno(self, *args, **kwargs):
+        resultado = metodo(self, *args, **kwargs)
+        
+        if 'Terra' in resultado:
+            return f'Você está em casa!'
+        return resultado    
+    return interno
+
 @add_repr
 class Times:
     def __init__(self, nome):
@@ -15,7 +24,9 @@ class Times:
 class Planeta:
     def __init__(self, nome):
         self.nome = nome
-    
+    @meu_planeta
+    def falar_nome(self):
+        return f'O planeta é {self.nome}'
     
 
 brasil = Times('Brasil')
@@ -24,3 +35,5 @@ terra = Planeta('Terra')
 marte = Planeta('Marte')
 
 print(brasil, portugal, terra, marte)
+print(terra.falar_nome())
+print(marte.falar_nome())
