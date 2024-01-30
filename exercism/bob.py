ead440896b2cd8dd041a675d037e24192b03ffe5
Lucas Ribeiro <lucas.devlop94@gmail.com>
2024-01-30 17:45:30 -0300

    bob w/ error

---
+++ exercism/bob.py
@@ -0,0 +1,13 @@
+def response(hey_bob):
+    if hey_bob.isupper():
+        return 'Whoa, chill out!'
+    elif hey_bob.endswith('?') and hey_bob.isupper():
+        return 'Calm down, I know what I\'m doing!'
+    elif hey_bob.endswith('?'):
+        return 'Sure.'
+    elif hey_bob.isspace() or not hey_bob:
+        return 'Fine. Be that way!'
+    else:
+        return 'Whatever.'
+    
+print(response("WHAT'S GOING ON?"))
