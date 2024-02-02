7f7c7da2951d414b7b4bcece24aca3087bcc2800
Lucas Ribeiro <lucas.devlop94@gmail.com>
2024-02-02 16:28:40 -0300

    raindrops

---
+++ exercism/raindrops.py
@@ -0,0 +1,22 @@
+"""Your task is to convert a number into its corresponding raindrop sounds.
+
+If a given number:
+
+is divisible by 3, add "Pling" to the result.
+is divisible by 5, add "Plang" to the result.
+is divisible by 7, add "Plong" to the result.
+is not divisible by 3, 5, or 7, the result should be the number as a string."""
+
+def convert(number):
+    result = ""
+    if number % 3 == 0:
+        result += "Pling"
+    if number % 5 == 0:
+        result += "Plang"
+    if number % 7 == 0:
+        result += "Plong"
+    if result == "":
+        result = str(number)
+    return result
+
+print(convert(30))
