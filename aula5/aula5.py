09ee4c84803e341e160b10b20fd2fd1618c00944
Lucassribeiro9 <lucas.devlop94@gmail.com>
2024-08-02 14:59:39 -0300

    teste

--- aula5/aula5.py
+++ aula5/aula5.py
@@ -4,5 +4,5 @@
 print('Potenciacão **: ', 10 ** 5)
 print('Resto %: ', 10 % 5)
 print('Divisão inteira //: ', 10 // 50)
-print('Multiplicação * ', 10 * 'Beatriz') # Um número com uma string torna o multiplicador uma estrutura de repetição
+print('Multiplicação * ', 10 * 'Carlos') # Um número com uma string torna o multiplicador uma estrutura de repetição
 
