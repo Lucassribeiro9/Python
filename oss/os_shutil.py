7d409b8b6b4c7a2d0b17a2eb6c35092180dfa91e
Lucas Ribeiro <lucas.devlop94@gmail.com>
2025-03-19 15:22:23 -0300

    shutil rm

--- oss/os_shutil.py
+++ oss/os_shutil.py
@@ -17,12 +17,7 @@ DESKTOP = os.path.join(HOME, 'Desktop') # caminho da pasta desktop
 PASTA_ORIGINAL = os.path.join(DESKTOP, 'caju') # caminho da pasta que sera copiada
 NOVA_PASTA = os.path.join(DESKTOP, 'caju2') # caminho da pasta que sera criada
 
-# cria a pasta caso nao exista
-os.makedirs(NOVA_PASTA, exist_ok=True)
-
-# percorre a pasta original e seus subdiretorios
-for root, dirs, files in os.walk(PASTA_ORIGINAL):
-    for file in files: # percorre cada arquivo
-        caminho_arquivo = os.path.join(root, file) # monta o caminho do arquivo
-        shutil.copy(caminho_arquivo, NOVA_PASTA) # copia o arquivo para a pasta nova
-        print(f'Arquivo copiado: {caminho_arquivo}') # imprime o caminho do arquivo copiado
+#apaga toda a arvore da pasta
+shutil.rmtree(NOVA_PASTA)
+#copia a arvore da pasta por completo
+shutil.copytree(PASTA_ORIGINAL, NOVA_PASTA)
