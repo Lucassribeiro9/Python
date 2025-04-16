aa0d4d629aaadd2bdc3382f848544d629f320054
Lucas Ribeiro <lucas.devlop94@gmail.com>
2025-04-16 15:55:57 -0300

    modificando o script

--- atualizando planilhas/script_atualizacao.py
+++ atualizando planilhas/script_atualizacao.py
@@ -1,12 +1,7 @@
-import schedule
-import time
 from cpplanilhas import executar_atualizacao
 
 def atualizar_planilha():
     executar_atualizacao()
+    print("Planilha atualizada com sucesso!")
 
-schedule.every(7).days.at("08:00").do(atualizar_planilha)  # execute a cada 7 dias, às 08:00
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+atualizar_planilha()
