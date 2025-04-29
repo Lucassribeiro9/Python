31fc178747648f8db547c51810c7176cdc7cd58a
Lucas Ribeiro <lucas.devlop94@gmail.com>
2025-04-29 16:20:56 -0300

    adicionando encoding

--- webscraping/webscrap.py
+++ webscraping/webscrap.py
@@ -6,9 +6,10 @@ from bs4 import BeautifulSoup
 url = 'http://localhost:8080'
 response = requests.get(url)
 raw_html = response.text
-parsed_html = BeautifulSoup(raw_html, 'html.parser')
+parsed_html = BeautifulSoup(raw_html, 'html.parser', from_encoding='utf-8')
 
 print(parsed_html.prettify())
 if parsed_html.title.string is not None:
     print(parsed_html.title.string)
-print(parsed_html.find('h1').string)
+if parsed_html.find('h1').string is not None:
+    print(parsed_html.find('h1').string)
