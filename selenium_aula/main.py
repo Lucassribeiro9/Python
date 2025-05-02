df470d58284b46dc1c5e66597bf12e6753f3a056
Lucas Ribeiro <57766036+Lucassribeiro9@users.noreply.github.com>
2025-05-02 13:36:06 -0300

    find_element

--- selenium_aula/main.py
+++ selenium_aula/main.py
@@ -5,6 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 TIMEOUT = 10
 
@@ -16,8 +17,16 @@ def get_chrome_browser(site):
     return chrome_browser
 
 browser = get_chrome_browser('https://www.google.com')
+
+#Espera o elemento de pesquisa aparecer na página
 search_input = WebDriverWait(browser, TIMEOUT).until(
     EC.presence_of_element_located((By.NAME, 'q'))
 )
-search_input.send_keys('NBA')
+
+search_input.send_keys('Hello World')
+search_input.send_keys(Keys.ENTER)
+
+results = browser.find_element(By.ID, 'search')
+links = results.find_elements(By.TAG_NAME, 'a')
+links[0].click()
 sleep(TIMEOUT)
