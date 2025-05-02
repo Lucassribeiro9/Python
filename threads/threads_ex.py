e062734d9e0f4518d2b74bfab256d2f45db7f43a
Lucas Ribeiro <57766036+Lucassribeiro9@users.noreply.github.com>
2025-05-02 18:47:29 -0300

    threads p3

--- threads/threads_ex.py
+++ threads/threads_ex.py
@@ -1,29 +1,27 @@
 # processamentos paralelos com Python
 from threading import Thread
 from time import sleep
+from threading import Lock
 
-class MyThread(Thread):
-    def __init__(self, name, delay):
-        Thread.__init__(self)
-        self.name = name
-        self.delay = delay
-
-    def run(self):
-        print(f"Starting {self.name}")
-        sleep(self.delay)
-        print(f"Exiting {self.name}")
-
+class Ingressos:
+    def __init__(self, estoque):
+        self.estoque = estoque
+        self.lock = Lock()
     
-def vai_demorar(texto, tempo):
-        sleep(tempo)
-        print(texto)
-
-t1 = Thread(target=vai_demorar, args=("Thread 1", 2))
-t1.start()
-t2 = Thread(target=vai_demorar, args=("Thread 2", 4))
-t2.start()
-
-while t1.is_alive():
-    print("Thread 1 ainda está viva")
-    sleep(2)
-print("Thread 1 já morreu")
+    def comprar(self, quantidade):
+        self.lock.acquire()
+        if self.estoque < quantidade:
+            print(f"Não há ingressos suficientes. Estoque atual: {self.estoque}")
+            self.lock.release()
+            return
+        self.estoque -= quantidade
+        print(f"Comprando {quantidade} ingressos. Estoque atual: {self.estoque}")
+        self.lock.release()
+           
+           
+if __name__ == '__main__':
+    ingressos = Ingressos(10)
+    for i in range(1, 20):
+        t = Thread(target=ingressos.comprar, args=(1,))
+        t.start()
+        sleep(2)
