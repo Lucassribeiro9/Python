0e66a7cd161a8e4ebe843e9a332ee03aa6515a7e
Lucas Ribeiro <lucas.devlop94@gmail.com>
2025-05-21 15:11:06 -0300

    adicionando metodo setupTheme importando modulo styles

--- GUI/calculadora_g/main.py
+++ GUI/calculadora_g/main.py
@@ -4,10 +4,12 @@ from consts import WIN_ICON_PATH
 from display import Display
 from main_window import Info, MainWindow
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtWidgets import QApplication
+from styles import setupTheme
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    setupTheme(app)
     window = MainWindow()
 
     # Icon
