49a7846a5ffbbed69c52fe5ca64e47c5853bedd3
Lucas Ribeiro <lucas.devlop94@gmail.com>
2025-05-21 14:40:38 -0300

    adicionando consts

---
+++ GUI/calculadora_g/consts.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+FILES_DIR = ROOT_DIR / "files"
+WIN_ICON_PATH = FILES_DIR / "calc_icon.jfif"
+
+BIG_FONT = 40
+MEDIUM_FONT = 24
+SMALL_FONT = 19
+TEXT_MARGIN = 10
+MINIMUM_WIDTH = 300
