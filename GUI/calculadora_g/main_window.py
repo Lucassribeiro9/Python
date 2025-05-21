797337d392cf454ccc09fda8be63dbd5f7930124
Lucas Ribeiro <lucas.devlop94@gmail.com>
2025-05-21 14:41:43 -0300

    adicionando imports e criando classe para label

--- GUI/calculadora_g/main_window.py
+++ GUI/calculadora_g/main_window.py
@@ -1,5 +1,6 @@
-from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QWidget
-
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QLabel
+from consts import SMALL_FONT
 
 class MainWindow(QMainWindow):
     def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
@@ -14,6 +15,13 @@ class MainWindow(QMainWindow):
         self.adjustSize()
         self.setFixedSize(self.width(), self.height())
 
-    def addWidgetToLayout(self, widget):
+    def addToVLayout(self, widget):
         self.v_layout.addWidget(widget)
-        self.adjustFixedSize()
+
+class Info(QLabel):
+    def __init__(self, text: str, parent: QWidget | None = None) -> None:
+        super().__init__(text, parent)
+        self.configStyle()
+    def configStyle(self):
+        self.setStyleSheet(f'font-size: {SMALL_FONT}px;')
+        self.setAlignment(Qt.AlignmentFlag.AlignRight)
