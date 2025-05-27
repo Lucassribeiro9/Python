6f1bbf191c6418f3c7b79eeb30b773f3008e1564
Lucas Ribeiro <lucas.devlop94@gmail.com>
2025-05-27 15:02:00 -0300

    mudando de pasta

---
+++ GUI/calculadora_g/style/display.py
@@ -0,0 +1,16 @@
+from consts import BIG_FONT, MINIMUM_WIDTH, TEXT_MARGIN
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QLineEdit
+
+
+class Display(QLineEdit):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.configStyle()
+
+    def configStyle(self):
+        self.setStyleSheet(f"font-size: {BIG_FONT}px;")
+        self.setMinimumHeight(BIG_FONT * 2)
+        self.setMinimumWidth(MINIMUM_WIDTH)
+        self.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.setTextMargins(*[TEXT_MARGIN for _ in range(4)])
