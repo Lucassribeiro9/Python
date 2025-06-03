9d8a09e61c4dae0306ea423c20c88f0e172c3133
Lucas Ribeiro <lucas.devlop94@gmail.com>
2025-06-03 10:46:59 -0300

    adicionando regex e funcoes

--- GUI/calculadora_g/utils.py
+++ GUI/calculadora_g/utils.py
@@ -1,9 +1,11 @@
 import re
 
-NUM_OR_DOT = re.compile(r'^[0-9\.]')
+NUM_OR_DOT = re.compile(r"^[0-9\.]")
+
 
 def isNumOrDot(string: str):
     return bool(NUM_OR_DOT.search(string))
+
 
 def isEmpty(string: str):
     return len(string) == 0
