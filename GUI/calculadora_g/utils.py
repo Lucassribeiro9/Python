81956bfc49b5554dc6d745d761a2e6d61cef4eba
Lucas Ribeiro <lucas.devlop94@gmail.com>
2025-06-03 14:28:20 -0300

    criando a def isValidNumber

--- GUI/calculadora_g/utils.py
+++ GUI/calculadora_g/utils.py
@@ -6,6 +6,13 @@ NUM_OR_DOT = re.compile(r"^[0-9\.]")
 def isNumOrDot(string: str):
     return bool(NUM_OR_DOT.search(string))
 
+def isValidNumber(string: str):
+    try:
+        float(string)
+        valid = True
+    except ValueError:
+        valid = False
+    return valid
 
 def isEmpty(string: str):
     return len(string) == 0
