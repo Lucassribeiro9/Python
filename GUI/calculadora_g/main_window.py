92204ddf425cb93197914d44ef8e7a8c6df33e71
Lucassribeiro9 <lucas.devlop94@gmail.com>
2025-06-05 15:32:52 -0300

    excluindo info

--- GUI/calculadora_g/main_window.py
+++ GUI/calculadora_g/main_window.py
@@ -17,11 +17,3 @@ class MainWindow(QMainWindow):
 
     def addWidgetToVLayout(self, widget):
         self.vLayout.addWidget(widget)
-
-class Info(QLabel):
-    def __init__(self, text: str, parent: QWidget | None = None) -> None:
-        super().__init__(text, parent)
-        self.configStyle()
-    def configStyle(self):
-        self.setStyleSheet(f'font-size: {SMALL_FONT}px;')
-        self.setAlignment(Qt.AlignmentFlag.AlignRight)
