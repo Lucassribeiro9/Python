3b231b676638081a00641e577ade6d454aa63d2c
Lucas Ribeiro <lucas.devlop94@gmail.com>
2025-06-26 17:13:35 -0300

    adiciona filtro de eventos para entrada de texto no campo de nome

--- GUI/qt_designer/ui/src/main_window.py
+++ GUI/qt_designer/ui/src/main_window.py
@@ -1,5 +1,8 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import QEvent, QObject, Qt
+from PySide6.QtGui import QKeyEvent
+from typing import cast
 from window import Ui_MainWindow
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -11,6 +14,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def changeLabelResult(self):
         text = self.inputName.text()
         self.labelResult.setText(f"Ce ta bem, {text}?")
+    def eventFilter(self, watched: QObject, event: QEvent) -> bool:
+        if event.type() == QEvent.Type.KeyPress:
+            event = cast(QKeyEvent, event)
+            self.inputName.setText(event.text())
+        return super().eventFilter(watched, event)
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     mainWindow = MainWindow()
