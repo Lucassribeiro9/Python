4ce3c2a8e4e0d96f7663112264d4eea329fd9065
Lucas Ribeiro <lucas.devlop94@gmail.com>
2025-07-01 15:54:57 -0300

    cria tabela 'customers' no banco de dados SQLite

--- banco_de_dados/main.py
+++ banco_de_dados/main.py
@@ -4,11 +4,18 @@ from pathlib import Path
 ROOT_FOLDER = Path(__file__).parent
 DB_NAME = "sqlite.db"
 DB_FILE = ROOT_FOLDER / DB_NAME
+TABLE_NAME = "customers"
 
 # Criando conexão com o banco de dados
 
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
+cursor.execute(
+    f"CREATE TABLE IF NOT EXISTS {TABLE_NAME} (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INTEGER)"
+)
+
+
+# Fechando conexão com o banco de dados
 cursor.close()
 connection.close()
