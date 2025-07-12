39d27971a7e83f62ff420c33c035e2b4a0256735
Lucassribeiro9 <lucas.devlop94@gmail.com>
2025-07-12 19:06:09 -0300

    refatorar importações e adicionar execução de criação de tabela no banco de dados

--- banco_de_dados/main.py
+++ banco_de_dados/main.py
@@ -1,13 +1,14 @@
+import os
+
 import pymysql
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 # Load environment variables from .env file
-MYSQL_HOST = os.getenv('MYSQL_HOST')
-MYSQL_USER = os.getenv('MYSQL_USER')
-MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
-MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
 
 connection = pymysql.connect(
     host=MYSQL_HOST,
@@ -18,4 +19,7 @@ connection = pymysql.connect(
 
 with connection:
     with connection.cursor() as cursor:
-        print("Connected to the database successfully!")
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50) NOT NULL, age INT NOT NULL)"
+        )
+        print(cursor)
