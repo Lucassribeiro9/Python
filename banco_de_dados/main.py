f36c24ca2aa3f1b72b5a951c975b58d84962382e
Lucas Ribeiro <lucas.devlop94@gmail.com>
2025-07-25 12:25:17 -0300

    update

--- banco_de_dados/main.py
+++ banco_de_dados/main.py
@@ -24,39 +24,6 @@ with connection:
             f"CREATE TABLE IF NOT EXISTS {TABLE_NAME} (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50) NOT NULL, age INT NOT NULL)"
         )
     connection.commit()
-    # insert
-    with connection.cursor() as cursor:
-        sql = f"INSERT INTO {TABLE_NAME} (name, age) VALUES (%s, %s)"
-        data2 = (
-            ("Lauro", 30),
-            ("Maria", 25),
-            ("João", 40),
-            ("Ana", 35),
-            ("Pedro", 28),
-            ("Carla", 32),
-            ("Rafael", 29),
-            ("Fernanda", 31),
-            ("Roberto", 45),
-            ("Patrícia", 27),
-        )
-        result = cursor.executemany(sql, data2)
-        connection.commit()
-    print("Inserted customer")
-    # select
-    with connection.cursor() as cursor:
-        select = f"SELECT * FROM {TABLE_NAME} WHERE age > 25"
-        result = cursor.execute(select)
-        customers = cursor.fetchall()
-        for customer in customers:
-            print(customer)
-    # delete
-    with connection.cursor() as cursor:
-        delete = f"DELETE FROM {TABLE_NAME} WHERE id < 30"
-        result = cursor.execute(delete)
-        connection.commit()
-        customers = cursor.fetchall()
-        for customer in customers:
-            print(customer)
     # update
     with connection.cursor() as cursor:
         update = f"UPDATE {TABLE_NAME} SET age = 30 WHERE id = 1"
