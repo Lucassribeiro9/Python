bcd52f554c61ea1d75be0c920c3f3864e4c3f802
Lucassribeiro9 <lucas.devlop94@gmail.com>
2025-08-04 10:23:31 -0300

    refatora estrutura de templates e URLs, adiciona menu de navegação e corrige blocos de conteúdo

--- proj_django/djangoproj/urls.py
+++ proj_django/djangoproj/urls.py
@@ -19,7 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('home.urls')),
-    path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('blog/exemplo/', include('blog.urls')),
+    path('admin/', admin.site.urls),
 ]
