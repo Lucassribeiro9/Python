70c53338638b91bd710632715dd44671bee85111
Lucas Ribeiro <57766036+Lucassribeiro9@users.noreply.github.com>
2025-08-11 15:28:24 -0300

    criando urls dinamicas com URL Dispatcher

--- proj_django/blog/views.py
+++ proj_django/blog/views.py
@@ -11,3 +11,8 @@ def exemplo(request):
     print("Exemplo view")
     context = {"title": "Exemplo Page", "message": "This is an example page!"}
     return render(request, 'blog/exemplo.html', context)
+
+def post(request, id):
+    print(f"Post requested with ID: {id}")
+    context = {"posts": posts}
+    return render(request, 'blog/index.html', context)
